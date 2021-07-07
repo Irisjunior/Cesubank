b12a959a9642ca2111d5325065a640fa5e7163a5
Iris Leite Vieira Junior <irisjunior1@gmail.com>
2021-07-07 17:36:47 -0300

    upload de arquivos

---
+++ README.md
@@ -0,0 +1,34 @@
+
+# Sistema de gerenciamento de senhas 
+Sistema desenvolvido para o gerenciamento de senhas por tipo de atendimento, são três tipos de atendiomento "Prioridade", "Rapido" e "Comum" e todos os caixas tem uma logica para a chamada das respectivas senhas.
+
+## Logicas dos caixas
+- __Caixa 1__:Realiza os atendimentos preferenciais. Se não houver atendimentos preferenciais na fila, atende o próximo atendimento rápido. Se não houver também, atende o próximo comum. Se não houver nenhum, não faz nada
+- __Caixa 2 e 3__:Atendem primeiro as senhas de atendimento rápido, se não houver atende o próximo da atendimento prioritário. Se não houver também, atende o próximo comum. Se não houver nenhum, não faz nada.
+- __Caixa 4__: Realiza os atendimentos comuns. Se não houver atendimentos comuns na fila, atende o próximo atendimento prioritário. Se não houver também, atende o próximo rápido. Se não houver nenhum, não faz nada.
+
+
+## 📦 Tecnologias utilizadas para construção:
+
+- HTML5 -  Como linguagem de marcação.
+- CSS3  -  Para estilização de todos os componentes.  
+- Javascript - para as funcionalidades de filas,caixas,painel de chamada e indicação da senha gerada.
+- Design Responsivo - Media Queries e Medidas Relativas.
+
+## 🖥 Preview do projeto 
+
+![](./img/desktop.jpg)
+![](./img/mobile.jpg)
+![](./img/tablet.jpg)
+
+## 📋 Pré-requisitos
+
+Antes de qualquer coisa, você precisa ter instaldo o [Google Chrome](https://www.google.com/intl/pt-BR/chrome) e [Git](https://git-scm.com/downloads). 
+
+## 🔧 Como rodar a página
+
+Abra o terminal Git Bash no diretorio em que deseja salvar o projeto e cole o seguinte comando:
+ 
+```
+
+```
---
+++ script.js
@@ -0,0 +1,224 @@
+//SELECÃO DOS BOTOES-------------
+
+const senhachamada = document.querySelector(".senha");
+const caixachamada = document.querySelector(".caixa");
+const atendimentos = document.querySelector(".atendimentos");
+
+const botaosenhap = document.querySelector(".botao-p");
+
+const botaosenhar = document.querySelector(".botao-r");
+const botaosenhac = document.querySelector(".botao-c");
+const suaSenha = document.querySelector(".sua-senha");
+
+const botaocaixa = document.querySelector(".botao");
+const botaocaixa2 = document.getElementById("caixa2");
+const botaocaixa3 = document.getElementById("caixa3");
+const botaocaixa4 = document.getElementById("caixa4");
+/*--------------------------------------------------------*/
+// Declaração dos arrays filas
+let filaPrioridade = [];
+let filaRapido = [];
+let filaComum = [];
+
+// variavel contadora das filas
+let cont = 0;
+
+//FUNCOES DE ADICIONAR FILAS--------
+//FUNCAO ADICIONAR FILA PRIORITARIO:
+function adicionarfilaPrioritario(event) {
+  if (event.target.innerText === "Prioritario") {
+    cont++;
+    filaPrioridade.push(`P-${cont}`);
+    console.log(filaPrioridade);
+    suaSenha.innerHTML = `<p>Sua senha é: </p><h2>P-${cont}</h2>`;
+  }
+}
+
+//FUNCAO ADICIONAR FILA RAPIDO------------
+function adicionarfilaRapido(event) {
+  if (event.target.innerText === "Rápido") {
+    cont++;
+    filaRapido.push(`R-${cont}`);
+    console.log(filaRapido);
+    suaSenha.innerHTML = `<p>Sua senha é:</p><h2>R-${cont}</h2>`;
+  }
+}
+// FUNCAO ADICIONAR FILA COMUM--------------
+function adicionarfilaComum(event) {
+  if (event.target.innerText === "Comum") {
+    cont++;
+    filaComum.push(`C-${cont}`);
+    console.log(filaComum);
+    suaSenha.innerHTML = `<p>Sua senha é: </p><h2>C-${cont}</h2>`;
+  }
+}
+
+// CODIGO DA SINALEIRA -----------------------
+
+let context,
+  oscillator,
+  contextGain,
+  x = 0;
+
+function start() {
+  context = new AudioContext();
+  oscillator = context.createOscillator();
+  contextGain = context.createGain();
+
+  oscillator.connect(contextGain);
+  contextGain.connect(context.destination);
+  oscillator.start(0);
+}
+
+function stop() {
+  start();
+  contextGain.gain.exponentialRampToValueAtTime(
+    0.000001,
+    context.currentTime + x
+  );
+}
+
+/*
+CAIXA 1 PRIORITARIO,RAPIDO E COMUM-----------------------
+Realiza os atendimentos preferenciais. Se não houver atendimentos preferenciais na fila, atende 
+o próximo atendimento rápido. Se não houver também, atende o próximo comum. Se não houver nenhum,
+não faz nada.
+*/
+function chamarSenha(event) {
+  if (event.target.innerText === "Caixa 1" && filaPrioridade.length > 0) {
+    var chamada = filaPrioridade.shift();
+    senhachamada.innerHTML = `<h1>${chamada}</h1>`;
+    caixachamada.innerHTML = `<h2>CAIXA 1</h2>`;
+    console.log(filaPrioridade);
+    x = 5;
+    stop();
+  } else if (filaRapido.length > 0 && event.target.innerText === "Caixa 1") {
+    var chamada = filaRapido.shift();
+    senhachamada.innerHTML = `<h1>${chamada}</h1>`;
+    caixachamada.innerHTML = `<h2>CAIXA 1</h2>`;
+    console.log(filaRapido);
+    x = 5;
+    stop();
+  } else if (filaComum.length > 0 && event.target.innerText === "Caixa 1") {
+    var chamada = filaComum.shift();
+    senhachamada.innerHTML = `<h1>${chamada}</h1>`;
+    caixachamada.innerHTML = `<h2>CAIXA 1</h2>`;
+    console.log(filaComum);
+    x = 5;
+    stop();
+  }
+}
+
+//Caixas 2 e 3: Atendem primeiro as senhas de atendimento rápido, se não houver atende o próximo da atendimento prioritário. Se não houver também, atende o próximo comum. Se não houver nenhum, não faz nada.
+
+function chamarSenha2(event) {
+  if (event.target.innerText === "Caixa 2" && filaRapido.length > 0) {
+    var chamada = filaRapido.shift();
+    senhachamada.innerHTML = `<h1>${chamada}</h1>`;
+    caixachamada.innerHTML = `<h2>CAIXA 2</h2>`;
+    console.log(filaRapido);
+    x = 5;
+    stop();
+  } else if (
+    filaPrioridade.length > 0 &&
+    event.target.innerText === "Caixa 2"
+  ) {
+    var chamada = filaPrioridade.shift();
+    senhachamada.innerHTML = `<h1>${chamada}</h1>`;
+    caixachamada.innerHTML = `<h2>CAIXA 2</h2>`;
+    console.log(filaPrioridade);
+    x = 5;
+    stop();
+  } else if (filaComum.length > 0 && event.target.innerText === "Caixa 2") {
+    var chamada = filaComum.shift();
+    senhachamada.innerHTML = `<h1>${chamada}</h1>`;
+    caixachamada.innerHTML = `<h2>CAIXA 2</h2>`;
+    console.log(filaComum);
+    x = 5;
+    stop();
+  }
+}
+
+//CAIXA 3 ------------------------------
+
+function chamarSenha3(event) {
+  if (event.target.innerText === "Caixa 3" && filaRapido.length > 0) {
+    var chamada = filaRapido.shift();
+    senhachamada.innerHTML = `<h1>${chamada}</h1>`;
+    caixachamada.innerHTML = `<h2>CAIXA 3</h2>`;
+    console.log(filaPrioridade);
+    x = 5;
+    stop();
+  } else if (
+    filaPrioridade.length > 0 &&
+    event.target.innerText === "Caixa 3"
+  ) {
+    var chamada = filaPrioridade.shift();
+    senhachamada.innerHTML = `<h1>${chamada}</h1>`;
+    caixachamada.innerHTML = `<h2>CAIXA 3</h2>`;
+    console.log(filaRapido);
+    x = 5;
+    stop();
+  } else if (filaComum.length > 0 && event.target.innerText === "Caixa 3") {
+    var chamada = filaComum.shift();
+    senhachamada.innerHTML = `<h1>${chamada}</h1>`;
+    caixachamada.innerHTML = `<h2>CAIXA 3</h2>`;
+    console.log(filaComum);
+    x = 5;
+    stop();
+  }
+}
+
+//  CAIXA 4 ---------------------------------
+
+function chamarSenha4(event) {
+  if (event.target.innerText === "Caixa 4" && filaComum.length > 0) {
+    var chamada = filaComum.shift();
+    senhachamada.innerHTML = `<h1>${chamada}</h1>`;
+    caixachamada.innerHTML = `<h2>CAIXA 4</h2>`;
+    console.log(filaPrioridade);
+    x = 5;
+    stop();
+  } else if (
+    filaPrioridade.length > 0 &&
+    event.target.innerText === "Caixa 4"
+  ) {
+    var chamada = filaPrioridade.shift();
+    senhachamada.innerHTML = `<h1>${chamada}</h1>`;
+    caixachamada.innerHTML = `<h2>CAIXA 4</h2>`;
+    console.log(filaPrioridade);
+    x = 5;
+    stop();
+  } else if (filaRapido.length > 0 && event.target.innerText === "Caixa 4") {
+    var chamada = filaRapido.shift();
+    senhachamada.innerHTML = `<h1>${chamada}</h1>`;
+    caixachamada.innerHTML = `<h2>CAIXA 4</h2>`;
+    console.log(filaRapido);
+    x = 5;
+    stop();
+  }
+}
+
+botaosenhap.addEventListener("click", function (event) {
+  adicionarfilaPrioritario(event);
+});
+
+botaosenhar.addEventListener("click", function (event) {
+  adicionarfilaRapido(event);
+});
+botaosenhac.addEventListener("click", function (event) {
+  adicionarfilaComum(event);
+});
+
+botaocaixa.addEventListener("click", function (event) {
+  chamarSenha(event);
+});
+botaocaixa2.addEventListener("click", function (event) {
+  chamarSenha2(event);
+});
+botaocaixa3.addEventListener("click", function (event) {
+  chamarSenha3(event);
+});
+botaocaixa4.addEventListener("click", function (event) {
+  chamarSenha4(event);
+});
